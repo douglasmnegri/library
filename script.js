37e4781a39e74d0708bf3f229f4afc254a131391
Douglas Melo <douglasmnegri@gmail.com>
2024-02-21 21:37:48 -0300

    create a template to test the functionality

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ function Book() {
   // the constructor...
 }
 
+const bookTitle = document.querySelector("#book-title");
+const bookPages = document.querySelector("#book-pages");
+const button = document.querySelector("#btn");
+
 function addBookToLibrary() {
   // do stuff here
 }
+button.addEventListener("click", () => {
+  console.log(bookTitle.value, bookPages.value);
+});
