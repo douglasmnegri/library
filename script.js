002d481a8f4f88bea6c4e6a24065fdf4b8a27aa6
Douglas Melo <douglasmnegri@gmail.com>
2024-02-25 16:46:13 -0300

    add the logic to receive the user's inputs

--- script.js
+++ script.js
@@ -1,16 +1,51 @@
 const myLibrary = [];
+// { title: "Nineteen Eigthy-four", pages: 335, read: false }
 
-function Book() {
-  // the constructor...
+
+function Book(title, pages, read) {
+  this.title = title;
+  this.pages = pages;
+  this.read = read;
 }
 
-const bookTitle = document.querySelector("#book-title");
-const bookPages = document.querySelector("#book-pages");
-const button = document.querySelector("#btn");
+Book.prototype.addBookToLibrary = addBookToLibrary;
+Book.prototype.listOfBooks = listOfBooks;
+
+function listOfBooks() {
+  const button = document.getElementById("btn");
+
+  button.addEventListener("click", (e) => {
+    e.preventDefault();
+    title = document.getElementById("book-title").value;
+    pages = document.getElementById("book-pages").value;
+    read = document.getElementById("status-read").checked;
+    const bookToAdd = new Book(title, pages, read);
+    bookToAdd.addBookToLibrary();
+    printBook();
+  });
+}
 
 function addBookToLibrary() {
-  // do stuff here
+  myLibrary.push(this);
 }
-button.addEventListener("click", () => {
-  console.log(bookTitle.value, bookPages.value);
-});
+
+listOfBooks()
+
+function printBook() {
+  const bookContainer = document.querySelector(".list-of-books");
+  for (let i = 0; i < myLibrary.length; i++) {
+    const bookExample = document.createElement("li");
+    const title = document.createElement("div");
+    const pages = document.createElement("div");
+    const read = document.createElement("div");
+
+    title.textContent = `Title: ${myLibrary[i].title}`;
+    pages.textContent = `Pages: ${myLibrary[i].pages}`;
+    read.textContent = `Read: ${myLibrary[i].read}`;
+
+    bookExample.appendChild(title);
+    title.appendChild(pages);
+    pages.appendChild(read);
+    bookContainer.appendChild(bookExample);
+  }
+}
